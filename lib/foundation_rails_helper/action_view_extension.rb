9c7478840e2f0e376755c5251d5b08d502c59084
Sébastien Gruhier <sebastien.gruhier@xilinus.com>
2012-01-15 14:12:23 +0100

    allows other form builder, just use foundation if nothing is set

--- lib/foundation_rails_helper/action_view_extension.rb
+++ lib/foundation_rails_helper/action_view_extension.rb
@@ -2,14 +2,14 @@ module ActionView
   module Helpers
     module FormHelper
       def form_for_with_foundation(record, options = {}, &block)
-        options[:builder] = FoundationRailsHelper::FormBuilder
+        options[:builder] ||= FoundationRailsHelper::FormBuilder
         options[:html] ||= {}
         options[:html][:class] ||= 'nice'
         form_for_without_foundation(record, options, &block) 
       end
       
       def fields_for_with_foundation(record_name, record_object = nil, options = {}, &block)
-        options[:builder] = FoundationRailsHelper::FormBuilder
+        options[:builder] ||= FoundationRailsHelper::FormBuilder
         options[:html] ||= {}
         options[:html][:class] ||= 'nice'
         fields_for_without_foundation(record_name, record_object, options, &block)
