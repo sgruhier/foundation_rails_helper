184e2fae343a5ebe2415c6771b5e85cbfbf2982d
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2014-05-10 21:30:50 -0700

    Add .to_sym

--- lib/foundation_rails_helper/flash_helper.rb
+++ lib/foundation_rails_helper/flash_helper.rb
@@ -18,7 +18,7 @@ module FoundationRailsHelper
       key_matching = DEFAULT_KEY_MATCHING.merge(key_matching)
       
       flash.inject "" do |message, (key, value)|
-        message += content_tag :div, :data => { :alert => "" }, :class => "alert-box #{key_matching[key] || :standard}" do
+        message += content_tag :div, :data => { :alert => "" }, :class => "alert-box #{key_matching[key.to_sym] || :standard}" do
           (value + link_to("&times;".html_safe, "#", :class => :close)).html_safe
         end
       end.html_safe
