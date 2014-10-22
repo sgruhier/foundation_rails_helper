bcbdf0755a3eb6401bf879dd35d11fedc384988e
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2014-10-21 22:08:07 -0700

    Add tests for text field class option

--- lib/foundation_rails_helper/form_builder.rb
+++ lib/foundation_rails_helper/form_builder.rb
@@ -137,7 +137,7 @@ module FoundationRailsHelper
       html = ''.html_safe
       html = custom_label(attribute, options[:label], options[:label_options]) if @options[:auto_labels] || options[:label]
       class_options = html_options || options
-      class_options[:class] = "#{class_options[:class]}"
+      class_options[:class] = class_options[:class].to_s
       class_options[:class] += " error" if has_error?(attribute)
       options.delete(:label)
       options.delete(:label_options)
