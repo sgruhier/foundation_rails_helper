7773b35b8474c85a574603e709f31cc141d82a4b
Duncan Stuart <dgmstuart@gmail.com>
2015-04-30 00:37:42 +0100

    Refactoring: Strengthen the flash helper specs - ensure they check for values as well as classes, and add a spec to check that multiple flash messages are generated correctly.
    Then refactor the specs to use capture/concat rather than appending to
    strings and using html_safe. See
    http://thepugautomatic.com/2013/06/helpers/#comment-1417845807
    for the approach.
    
    Also factor logic out into methods for clarity
    
    ActionView::Helpers::TextHelper is included in the spec so that RSpec
    can access the concat method.

--- spec/foundation_rails_helper/flash_helper_spec.rb
+++ spec/foundation_rails_helper/flash_helper_spec.rb
@@ -6,33 +6,43 @@ describe FoundationRailsHelper::FlashHelper do
   include ActionView::Context if defined?(ActionView::Context)
   include ActionView::Helpers::UrlHelper
   include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::TextHelper
   include FoundationRailsHelper::FlashHelper
 
   FoundationRailsHelper::FlashHelper::DEFAULT_KEY_MATCHING.each do |message_type, foundation_type|
     it "displays flash message with #{foundation_type} class for #{message_type} message" do
       allow(self).to receive(:flash).and_return({message_type => "Flash message"})
       node = Capybara.string display_flash_messages
-      expect(node).to have_css("div.alert-box.#{foundation_type}", :text => "Flash message")
-      expect(node).to have_css("div.alert-box a.close", :text => "×")
+      expect(node).
+        to  have_css("div.alert-box.#{foundation_type}", :text => "Flash message").
+        and have_css("div.alert-box a.close", :text => "×")
     end
+  end
+
+  it "displays multiple flash messages" do
+    allow(self).to receive(:flash).and_return({ success: "Yay it worked", error: "But this other thing failed" })
+    node = Capybara.string display_flash_messages
+    expect(node).
+      to  have_css("div.alert-box.success", :text => "Yay it worked").
+      and have_css("div.alert-box.alert",   :text => "But this other thing failed")
   end
 
   it "displays flash message with overridden key matching" do
     allow(self).to receive(:flash).and_return({:notice => "Flash message"})
     node = Capybara.string display_flash_messages({:notice => :alert})
-    expect(node).to have_css("div.alert-box.alert")
+    expect(node).to have_css("div.alert-box.alert", :text => "Flash message")
   end
 
   it "displays flash message with custom key matching" do
     allow(self).to receive(:flash).and_return({:custom_type => "Flash message"})
     node = Capybara.string display_flash_messages({:custom_type => :custom_class})
-    expect(node).to have_css("div.alert-box.custom_class")
+    expect(node).to have_css("div.alert-box.custom_class", :text => "Flash message")
   end
 
   it "displays flash message with standard class if key doesn't match" do
     allow(self).to receive(:flash).and_return({:custom_type => "Flash message"})
     node = Capybara.string display_flash_messages
-    expect(node).to have_css("div.alert-box.standard")
+    expect(node).to have_css("div.alert-box.standard", :text => "Flash message")
   end
   
   it "shouldn't die when flash hash contains devise internal data" do
