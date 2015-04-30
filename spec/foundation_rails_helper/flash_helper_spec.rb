a953a802e0dba70d3dab21d131d1c783580b837e
Duncan Stuart <dgmstuart@gmail.com>
2015-04-30 02:04:58 +0100

    Change specs to use string keys: this is what we actually get when looping through the FlashHash

--- spec/foundation_rails_helper/flash_helper_spec.rb
+++ spec/foundation_rails_helper/flash_helper_spec.rb
@@ -11,7 +11,7 @@ describe FoundationRailsHelper::FlashHelper do
 
   FoundationRailsHelper::FlashHelper::DEFAULT_KEY_MATCHING.each do |message_type, foundation_type|
     it "displays flash message with #{foundation_type} class for #{message_type} message" do
-      allow(self).to receive(:flash).and_return({message_type => "Flash message"})
+      allow(self).to receive(:flash).and_return({message_type.to_s => "Flash message"})
       node = Capybara.string display_flash_messages
       expect(node).
         to  have_css("div.alert-box.#{foundation_type}", :text => "Flash message").
@@ -19,8 +19,20 @@ describe FoundationRailsHelper::FlashHelper do
     end
   end
 
+  it "handles symbol keys" do
+    allow(self).to receive(:flash).and_return({ "success" => "Flash message" })
+    node = Capybara.string display_flash_messages
+    expect(node).to have_css("div.alert-box.success", :text => "Flash message")
+  end
+
+  it "handles string keys" do
+    allow(self).to receive(:flash).and_return({ "success" => "Flash message" })
+    node = Capybara.string display_flash_messages
+    expect(node).to have_css("div.alert-box.success", :text => "Flash message")
+  end
+
   it "displays multiple flash messages" do
-    allow(self).to receive(:flash).and_return({ success: "Yay it worked", error: "But this other thing failed" })
+    allow(self).to receive(:flash).and_return({ "success" => "Yay it worked", "error" => "But this other thing failed" })
     node = Capybara.string display_flash_messages
     expect(node).
       to  have_css("div.alert-box.success", :text => "Yay it worked").
@@ -28,19 +40,19 @@ describe FoundationRailsHelper::FlashHelper do
   end
 
   it "displays flash message with overridden key matching" do
-    allow(self).to receive(:flash).and_return({:notice => "Flash message"})
+    allow(self).to receive(:flash).and_return({ "notice" => "Flash message" })
     node = Capybara.string display_flash_messages({:notice => :alert})
     expect(node).to have_css("div.alert-box.alert", :text => "Flash message")
   end
 
   it "displays flash message with custom key matching" do
-    allow(self).to receive(:flash).and_return({:custom_type => "Flash message"})
+    allow(self).to receive(:flash).and_return({ "custom_type" => "Flash message" })
     node = Capybara.string display_flash_messages({:custom_type => :custom_class})
     expect(node).to have_css("div.alert-box.custom_class", :text => "Flash message")
   end
 
   it "displays flash message with standard class if key doesn't match" do
-    allow(self).to receive(:flash).and_return({:custom_type => "Flash message"})
+    allow(self).to receive(:flash).and_return({ "custom_type" => "Flash message" })
     node = Capybara.string display_flash_messages
     expect(node).to have_css("div.alert-box.standard", :text => "Flash message")
   end
