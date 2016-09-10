641ef6c691dd81ddb46293ce332e7e4fff82343c
Duncan Stuart <dgmstuart@gmail.com>
2016-09-10 16:05:29 +0100

    (refactor): Set FormBuilder as the default
    This is equivalent to the user setting
    
        config.action_view.default_form_builder = FoundationRailsHelper::FormBuilder
    
    ...in their application.
    
    This allows us to get rid of `alias_method_chain`, which is a
    (now-deprecated) method of monkeypatching
    ActionView::Helpers::FormHelper directly.
    
    For a detailed description of this issue, see
    https://github.com/sgruhier/foundation_rails_helper/issues/131

--- lib/foundation_rails_helper/action_view_extension.rb
+++ lib/foundation_rails_helper/action_view_extension.rb
@@ -1,21 +1,4 @@
-module ActionView
-  module Helpers
-    module FormHelper
-      def form_for_with_foundation(record, options = {}, &block)
-        options[:builder] ||= FoundationRailsHelper::FormBuilder
-        form_for_without_foundation(record, options, &block)
-      end
-
-      def fields_for_with_foundation(record_name, record_object = nil, options = {}, &block)
-        options[:builder] ||= FoundationRailsHelper::FormBuilder
-        fields_for_without_foundation(record_name, record_object, options, &block)
-      end
-
-      alias_method_chain :form_for, :foundation
-      alias_method_chain :fields_for, :foundation
-    end
-  end
-end
+ActionView::Base.default_form_builder = FoundationRailsHelper::FormBuilder
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
   html_tag
 end
