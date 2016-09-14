eacc83dcc7bd3200c004084aa2c5db6699d25266
Darrell Sandstrom <dsandstrom@einsteinmedical.com>
2016-09-14 11:03:04 -0700

    Change default button class to "success button"
    Remove radius(no longer used) and small(too small)

--- spec/foundation_rails_helper/configuration_spec.rb
+++ spec/foundation_rails_helper/configuration_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe FoundationRailsHelper do
   describe FoundationRailsHelper::Configuration do
     describe "#button_class" do
-      it "default value is 'small radius success button'" do
+      it "default value is 'success button'" do
         config = FoundationRailsHelper::Configuration.new
-        expect(config.button_class).to eq('small radius success button')
+        expect(config.button_class).to eq('success button')
       end
     end
 
@@ -41,7 +41,7 @@ describe FoundationRailsHelper do
         FoundationRailsHelper.reset
 
         config = FoundationRailsHelper.configuration
-        expect(config.button_class).to eq('small radius success button')
+        expect(config.button_class).to eq('success button')
       end
 
       it "resets the configured ignored flash keys" do
