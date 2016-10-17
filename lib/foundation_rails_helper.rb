be6fe60804bbf31bc77cfcd3dd5e0dd99cb37350
Oriol Gual <oriol.gual@gmail.com>
2016-10-17 11:07:12 +0200

    Extract class into own file to prevent parent class from having too many lines

--- lib/foundation_rails_helper.rb
+++ lib/foundation_rails_helper.rb
@@ -1,6 +1,7 @@
 require 'foundation_rails_helper/version'
 require 'foundation_rails_helper/configuration'
 require 'foundation_rails_helper/form_builder'
+require 'foundation_rails_helper/size_class_calculator'
 require 'foundation_rails_helper/flash_helper'
 require 'foundation_rails_helper/action_view_extension'
 ActiveSupport.on_load(:action_view) do
--- lib/foundation_rails_helper/form_builder.rb
+++ lib/foundation_rails_helper/form_builder.rb
@@ -139,38 +139,8 @@ module FoundationRailsHelper
     end
 
     def column_classes(options)
-      classes = SizeClassCalcluator.new(options).classes
+      classes = SizeClassCalculator.new(options).classes
       classes + ' columns'
-    end
-
-    class SizeClassCalcluator
-      def initialize(size_options)
-        @small = size_options[:small]
-        @medium = size_options[:medium]
-        @large = size_options[:large]
-      end
-
-      def classes
-        [small_class, medium_class, large_class].compact.join(' ')
-      end
-
-      private
-
-      def small_class
-        "small-#{@small}" if valid_size(@small)
-      end
-
-      def medium_class
-        "medium-#{@medium}" if valid_size(@medium)
-      end
-
-      def large_class
-        "large-#{@large}" if valid_size(@large)
-      end
-
-      def valid_size(value)
-        value.present? && value.to_i < 12
-      end
     end
 
     def tag_from_options(name, options)
